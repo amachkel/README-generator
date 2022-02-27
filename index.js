0175ef77321f53a09644c5acfacc7d0b7a99f4c2
amachkel <amachkel@gmail.com>
2022-02-26 19:10:01 -0500

    Added questions. Added README AC

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "What is the name of your project?",
+    },
+    {
+        type: "input",
+        name: "desc",
+        message: "What is a description of your project?",
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "What is required to install your project?",
+        default: "npm",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide instructions and examples for use.",
+    },
+    {
+        type: "input",
+        name: "credits",
+        message: "List collaborators here.",
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "Which license would you like to use?",
+        default: "MIT",
+    },
+])
+.then((data) => {
+    const fileName = "README.md";
+    writeToFile(fileName, data);
+});
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
