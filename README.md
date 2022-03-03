8721877bf32a12b1032089155eaef21e55813e32
amachkel <amachkel@gmail.com>
2022-03-03 11:20:45 -0500

    updated js to use constructor function to render license

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 asdf
 
+## Table of Contents
+ - [Description](#Description)
+
+
 ## Installation Requirements
 npm
 
@@ -9,13 +13,7 @@ npm
 asdf
 
 ## Credits
-asdf
+sadf
 
 ## License
-[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)## Table of Contents
- - [Description](#Description)
- - [Installation Requirements](#Installation Requirements)
- - [Usage](#Usage)
- - [Credits](#Credits)
- - [License](#License)
-
+[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,54 +1,67 @@
 // TODO: Create a function that returns a license badge and URL based on which license is passed in
 // If there is no license chosen, return an empty string
-function renderLicense(data) {
-  const license = data.license[0];
-  console.log(license);
-  const choices = [
-    {
-      name: "Apache 2.0",
-      badge: "Apache_2.0",
-      color: "blue",
-      url: "https://opensource.org/licenses/Apache-2.0",
-    },
-    {
-      name: "GNU Public v3.0",
-      badge: "GPLv3",
-      color: "blue",
-      url: "https://www.gnu.org/licenses/gpl-3.0",
-    },
-    {
-      name: "MIT",
-      badge: "MIT",
-      color: "yellow",
-      url: "https://opensource.org/licenses/MIT",
-    },
-    {
-      name: "Mozilla Public 2.0",
-      badge: "MPL_2.0",
-      color: "brightgreen",
-      url: "https://opensource.org/licenses/MPL-2.0",
-    },
-    {
-      name: "Boost Software License 1.0",
-      badge: "Boost_1.0",
-      color: "lightblue",
-      url: "https://www.boost.org/LICENSE_1_0.txt",
-    },
-    {
-      name: "The Unilicense",
-      badge: "Unlicense",
-      color: "blue",
-      url: "http://unlicense.org/",
-    },
-  ];
 
-  let match = choices.find((o) => o.name == license);
-  if (match != undefined && match != null) {
-    const generateLicense = `[![License](https://img.shields.io/badge/License-${match.badge}-${match.color}.svg)](${match.url})`;
+// const license = data.license[0];
+// console.log(license);
+function License(name, badge, color, url) {
+  this.name = name;
+  this.badge = badge;
+  this.color = color;
+  this.url = url;
+  this.licenseObj = () => {
+    const generateLicense = `[![License](https://img.shields.io/badge/License-${badge}-${color}.svg)](${url})`;
+    console.log(generateLicense);
     return `## License\n${generateLicense}`;
-  } else {
-    return "";
-  }
+  };
+}
+let choices = [];
+
+const apache = new License(
+  "Apache 2.0",
+  "Apache_2.0",
+  "blue",
+  "https://opensource.org/licenses/Apache-2.0"
+);
+choices.push(apache);
+const gnu = new License(
+  "GNU Public v3.0",
+  "GPLv3",
+  "blue",
+  "https://www.gnu.org/licenses/gpl-3.0"
+);
+choices.push(gnu);
+const mit = new License(
+  "MIT",
+  "MIT",
+  "yellow",
+  "https://opensource.org/licenses/MIT"
+);
+choices.push(mit);
+const moz = new License(
+  "Mozilla Public 2.0",
+  "MPL_2.0",
+  "brightgreen",
+  "https://opensource.org/licenses/MPL-2.0"
+);
+choices.push(moz);
+const boost = new License(
+  "Boost Software License 1.0",
+  "Boost_1.0",
+  "lightblue",
+  "https://www.boost.org/LICENSE_1_0.txt"
+);
+choices.push(boost);
+const uni = new License(
+  "The Unilicense",
+  "Unlicense",
+  "blue",
+  "http://unlicense.org/"
+);
+choices.push(uni);
+
+function renderLicense(data) {
+  let match = choices.find((item) => item.name == data.license);
+  if (match) return match.licenseObj();
 }
 
 // TODO: Create a function to generate markdown for README
@@ -65,7 +78,9 @@ function generateMarkdown(data) {
     markdown += `${data.desc}\n\n`;
     table += " - [Description](#Description)\n";
   }
-
+  if (data.table) {
+    markdown += `## Table of Contents\n${table}\n\n`;
+  }
   if (data.install) {
     markdown += `## Installation Requirements\n${data.install}\n\n`;
     table += " - [Installation Requirements](#Installation-Requirements)\n";
@@ -82,9 +97,7 @@ function generateMarkdown(data) {
     markdown += license;
     table += " - [License](#License)";
   }
-  if (data.table) {
-    markdown += `## Table of Contents\n${table}\n\n`;
-  }
+
   return markdown;
   return `# ${data.title}
 
@@ -113,3 +126,42 @@ function generateMarkdown(data) {
 // - [Credits](#Credits)
 // - [License](#License)
 module.exports = generateMarkdown;
+
+// const choices = [
+//   {
+//     name: "Apache 2.0",
+//     badge: "Apache_2.0",
+//     color: "blue",
+//     url: "https://opensource.org/licenses/Apache-2.0",
+//   },
+//   {
+//     name: "GNU Public v3.0",
+//     badge: "GPLv3",
+//     color: "blue",
+//     url: "https://www.gnu.org/licenses/gpl-3.0",
+//   },
+//   {
+//     name: "MIT",
+//     badge: "MIT",
+//     color: "yellow",
+//     url: "https://opensource.org/licenses/MIT",
+//   },
+//   {
+//     name: "Mozilla Public 2.0",
+//     badge: "MPL_2.0",
+//     color: "brightgreen",
+//     url: "https://opensource.org/licenses/MPL-2.0",
+//   },
+//   {
+//     name: "Boost Software License 1.0",
+//     badge: "Boost_1.0",
+//     color: "lightblue",
+//     url: "https://www.boost.org/LICENSE_1_0.txt",
+//   },
+//   {
+//     name: "The Unilicense",
+//     badge: "Unlicense",
+//     color: "blue",
+//     url: "http://unlicense.org/",
+//   },
+// ];
